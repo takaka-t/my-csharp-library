3a70c9259efeceefeeddc77a10666a95411a4695
‘takaka-t <86246662+takaka-t@users.noreply.github.com>
2022-04-11 21:26:15 +0900

    String型拡張クラス定義

---
+++ LibraryForBasicDataTypes/Extension/StringExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryForBasicDataTypes.Extension
+{
+    /// <summary>
+    /// String型拡張
+    /// </summary>
+    public static class StringExtension
+    {
+
+        /// <summary>
+        /// IsNullOrEmpty拡張
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this string val)
+        {
+            return string.IsNullOrEmpty(val);
+        }
+
+        /// <summary>
+        /// IsNullOrEmpty拡張
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static bool IsNullOrWhiteSpace(this string val)
+        {
+            return string.IsNullOrWhiteSpace(val);
+        }
+
+    }
+}
