e7de93157254cbfcfe5725efbe023df101b87711
‘takaka-t <86246662+takaka-t@users.noreply.github.com>
2022-04-11 21:51:16 +0900

    メインのライブラリとなるプロジェクト作成 プロジェクトの粒度で悩む

---
+++ LibraryOfMain/Helper/SeparatedValuesFiles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LibraryForSeparatedValuesFile.Helper
+{
+    public static class SeparatedValuesFiles
+    {
+
+        /// <summary>
+        /// 取込
+        /// </summary>
+        public static void Import()
+        {
+
+        }
+
+        /// <summary>
+        /// 出力
+        /// </summary>
+        public static void Export()
+        {
+
+        }
+
+
+        /// <summary>
+        /// 出力
+        /// </summary>
+        public static void Export<T>()
+        {
+
+        }
+
+
+    }
+}
