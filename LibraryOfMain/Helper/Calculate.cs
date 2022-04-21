1912d194760eecd79c38f72fe8f4490e043290a5
‘takaka-t <86246662+takaka-t@users.noreply.github.com>
2022-04-21 21:56:57 +0900

    Helper.Calculate内の関数実装 テストも完了

--- LibraryOfMain/Helper/Calculate.cs
+++ LibraryOfMain/Helper/Calculate.cs
@@ -10,24 +10,69 @@ namespace Helper
         /// <summary>
         /// 割合を計算する
         /// </summary>
-        /// <param name="target"></param>
-        /// <param name="percent"></param>
-        /// <returns>{target}の{percent}%</returns>
-        public static decimal MultiplyePercent(decimal target, decimal percent)
+        /// <param name="value">値</param>
+        /// <param name="percent">パーセント(%)</param>
+        /// <returns>{value}の{percent}%</returns>
+        public static decimal MultiplyePercent(decimal value, decimal percent)
         {
-            return target * ( percent / 100 );
-        } 
+            return value * ( percent / 100 );
+        }
 
         /// <summary>
         /// 指定した桁をFloor関数で切り捨てる
         /// </summary>
-        /// <param name="target"></param>
-        /// <param name="Digit"></param>
+        /// <param name="value">値</param>
+        /// <param name="digit">指定する桁</param>
         /// <returns></returns>
-        public static decimal FloorBySpecifieDigit(decimal target ,byte Digit)
+        public static decimal FloorBySpecifieDigit(decimal value ,sbyte digit)
         {
-            throw new NotImplementedException();
+            //var multiply = Math.Pow(digit, 10);
+            decimal multiply = 1;
+            for (sbyte i = 0; i < digit - 1; i++) { multiply *= 10;}
+            return Math.Floor(value * multiply) / multiply;
         }
 
+        /// <summary>
+        /// 指定した桁をTruncate関数で切り捨てる
+        /// </summary>
+        /// <param name="value">値</param>
+        /// <param name="digit">指定する桁</param>
+        /// <returns></returns>
+        public static decimal TruncateBySpecifieDigit(decimal value, sbyte digit)
+        {
+            //var multiply = Math.Pow(digit, 10);
+            decimal multiply = 1;
+            for (sbyte i = 0; i < digit - 1; i++) { multiply *= 10; }
+            return Math.Truncate(value * multiply) / multiply;
+        }
+
+        /// <summary>
+        /// 指定した桁をCeiling関数で切り上げる
+        /// </summary>
+        /// <param name="value">値</param>
+        /// <param name="digit">指定する桁</param>
+        /// <returns></returns>
+        public static decimal CeilingBySpecifieDigit(decimal value, sbyte digit)
+        {
+            //var multiply = Math.Pow(digit, 10);
+            decimal multiply = 1;
+            for (sbyte i = 0; i < digit - 1; i++) { multiply *= 10; }
+            return Math.Ceiling(value * multiply) / multiply;
+        }
+
+        /// <summary>
+        /// 指定した桁をRound関数で四捨五入する
+        /// ※mode変更可能
+        /// </summary>
+        /// <param name="value">値</param>
+        /// <param name="digit">指定する桁</param>
+        /// <returns></returns>
+        public static decimal RoundBySpecifieDigit(decimal value, sbyte digit, MidpointRounding mode = MidpointRounding.AwayFromZero)
+        {
+            //var multiply = Math.Pow(digit, 10);
+            decimal multiply = 1;
+            for (sbyte i = 0; i < digit - 1; i++) { multiply *= 10; }
+            return Math.Round(value * multiply, mode) / multiply;
+        }
     }
 }
--- LibraryOfMainTest/HelperTest/CalculateTest.cs
+++ LibraryOfMainTest/HelperTest/CalculateTest.cs
@@ -1,23 +1,162 @@
 ﻿using System;
 using Xunit;
+using Helper;
 
 namespace LibraryOfMainTest.HelperTest
 {
     public class CalculateTest
     {
 
-        [Fact]
-        public void MultiplyePercentTest()
+        public class MultiplyePercentTest
         {
-            decimal result = Helper.Calculate.MultiplyePercent(200, 20);
-            Assert.Equal(40, result);
+            [Fact]
+            public void Test1()
+            {
+                decimal result = Calculate.MultiplyePercent(200, 20);
+                Assert.Equal(40, result);
+            }
+            [Fact]
+            public void Test2()
+            {
+                decimal result = Calculate.MultiplyePercent(200, 120);
+                Assert.Equal(240, result);
+            }
+            [Fact]
+            public void Test3()
+            {
+                decimal result = Calculate.MultiplyePercent(200, (decimal)1.5);
+                Assert.Equal(3, result);
+            }
+            [Fact]
+            public void Test4()
+            {
+                decimal result = Calculate.MultiplyePercent(200, (decimal)1.1);
+                Assert.Equal((decimal)2.2, result);
+            }
+            [Fact]
+            public void Test5()
+            {
+                decimal result = Calculate.MultiplyePercent(200, -20);
+                Assert.Equal(-40, result);
+            }
         }
 
-        [Fact]
-        public void FloorBySpecifieDigitTest()
+        public class FloorBySpecifieDigitTest
         {
-            decimal result = Helper.Calculate.FloorBySpecifieDigit((decimal)123.456, 2);
-            Assert.Equal((decimal)123.4, result);
+            [Fact]
+            public void Test1()
+            {
+                decimal result = Calculate.FloorBySpecifieDigit((decimal)123.456, 2);
+                Assert.Equal((decimal)123.4, result);
+            }
+            [Fact]
+            public void Test2()
+            {
+                decimal result = Calculate.FloorBySpecifieDigit((decimal)123.456, 0);
+                Assert.Equal((decimal)123, result);
+            }
+            [Fact]
+            public void Test3()
+            {
+                decimal result = Calculate.FloorBySpecifieDigit((decimal)123.456, 4);
+                Assert.Equal((decimal)123.456, result);
+            }
+            [Fact]
+            public void Test4()
+            {
+                decimal result = Calculate.FloorBySpecifieDigit((decimal)-1.5, 1);
+                Assert.Equal((decimal)-2, result);  // Truncateとのマイナス値の丸め方の違い
+            }
+        }
+
+        public class TruncateBySpecifieDigitTest
+        {
+            [Fact]
+            public void Test1()
+            {
+                decimal result = Calculate.TruncateBySpecifieDigit((decimal)123.456, 2);
+                Assert.Equal((decimal)123.4, result);
+            }
+            [Fact]
+            public void Test2()
+            {
+                decimal result = Calculate.TruncateBySpecifieDigit((decimal)123.456, 0);
+                Assert.Equal((decimal)123, result);
+            }
+            [Fact]
+            public void Test3()
+            {
+                decimal result = Calculate.TruncateBySpecifieDigit((decimal)123.456, 4);
+                Assert.Equal((decimal)123.456, result);
+            }
+            [Fact]
+            public void Test4()
+            {
+                decimal result = Calculate.TruncateBySpecifieDigit((decimal)-1.5, 1);
+                Assert.Equal((decimal)-1, result); // Floorとのマイナス値の丸め方の違い
+            }
+        }
+
+        public class CeilingBySpecifieDigitTest
+        {
+            [Fact]
+            public void Test1()
+            {
+                decimal result = Calculate.CeilingBySpecifieDigit((decimal)123.456, 2);
+                Assert.Equal((decimal)123.5, result);
+            }
+            [Fact]
+            public void Test2()
+            {
+                decimal result = Calculate.CeilingBySpecifieDigit((decimal)123.456, 0);
+                Assert.Equal((decimal)124, result);
+            }
+            [Fact]
+            public void Test3()
+            {
+                decimal result = Calculate.CeilingBySpecifieDigit((decimal)123.456, 4);
+                Assert.Equal((decimal)123.456, result);
+            }
+            [Fact]
+            public void Test4()
+            {
+                decimal result = Calculate.CeilingBySpecifieDigit((decimal)-1.5, 1);
+                Assert.Equal((decimal)-1, result);
+            }
+        }
+
+        public class RoundBySpecifieDigitTest
+        {
+            [Fact]
+            public void Test1()
+            {
+                decimal result = Calculate.RoundBySpecifieDigit((decimal)123.456, 2);
+                Assert.Equal((decimal)123.5, result);
+            }
+            [Fact]
+            public void Test2()
+            {
+                decimal result = Calculate.RoundBySpecifieDigit((decimal)123.456, 0);
+                Assert.Equal((decimal)123, result);
+            }
+            [Fact]
+            public void Test3()
+            {
+                decimal result = Calculate.RoundBySpecifieDigit((decimal)123.456, 4);
+                Assert.Equal((decimal)123.456, result);
+            }
+            [Fact]
+            public void Test4()
+            {
+                decimal result = Calculate.RoundBySpecifieDigit((decimal)-1.4, 1);
+                Assert.Equal((decimal)-1, result); 
+            }
+            [Fact]
+            public void Test5()
+            {
+                decimal result = Calculate.RoundBySpecifieDigit((decimal)-1.5, 1);
+                Assert.Equal((decimal)-2, result);
+            }
         }
 
     }
